61113f0eb319a4362fbf9cadc515fd5136b9f94a
Michael Schout <mschout@gkg.net>
2011-05-30 16:50:41 -0500

    Build results of 31c1bd9 (on master)

--- lib/Task/BeLike/MSCHOUT.pm
+++ lib/Task/BeLike/MSCHOUT.pm
@@ -1,67 +1,111 @@
 package Task::BeLike::MSCHOUT;
+BEGIN {
+  $Task::BeLike::MSCHOUT::VERSION = '1.111500';
+}
 
 use strict;
 use warnings;
 
 # ABSTRACT: be more like MSCHOUT -- use the modules he does!
 
-=pkgroup Useful Command-Line Tools
 
-=pkg App::Ack 1.82 a working --pager
+__END__
+=pod
 
-=pkg App::Nopaste 0.28 Gist support via Gist API
+=head1 NAME
 
-=pkg App::cpanminus
+Task::BeLike::MSCHOUT - be more like MSCHOUT -- use the modules he does!
 
-=pkg App::cpanoutdated
+=head1 VERSION
 
-=pkg Perl::Tidy
+version 1.111500
 
-=pkg Vimana
+=head1 TASK CONTENTS
 
+=head2 Useful Command-Line Tools
 
-=pkgroup Useful CPAN Modules
+=head3 L<App::Ack> 1.82
 
-=pkg Archive::Zip
+Version 1.82 required because: a working --pager
 
-=pkg BackPAN::Index
+=head3 L<App::Nopaste> 0.28
 
-=pkg LWP::Protocol::https
+Version 0.28 required because: Gist support via Gist API
 
-=pkg CSS::Minifier
+=head3 L<App::cpanminus>
 
-=pkg CSS::Minifier::XS
+=head3 L<App::cpanoutdated>
 
-=pkg DBD::SQLite
+=head3 L<Perl::Tidy>
 
-=pkg DBI
+=head3 L<Vimana>
 
-=pkg Dist::Zilla::Plugin::Twitter
+=head2 Useful CPAN Modules
 
-=pkg Dist::Zilla::PluginBundle::MSCHOUT
+=head3 L<Archive::Zip>
 
-=pkg Dist::Zilla::PluginBundle::RJBS
+=head3 L<BackPAN::Index>
 
-=pkg Gravatar::URL
+=head3 L<LWP::Protocol::https>
 
-=pkg IO::Socket::SSL
+=head3 L<CSS::Minifier>
 
-=pkg IPC::System::Simple
+=head3 L<CSS::Minifier::XS>
 
-=pkg JavaScript::Minifier
+=head3 L<DBD::SQLite>
 
-=pkg JavaScript::Minifier::XS
+=head3 L<DBI>
 
-=pkg Modern::Perl
+=head3 L<Dist::Zilla::Plugin::Twitter>
 
-=pkg Pod::Coverage::TrustPod
+=head3 L<Dist::Zilla::PluginBundle::MSCHOUT>
 
-=pkg SQL::Beautify
+=head3 L<Dist::Zilla::PluginBundle::RJBS>
 
-=pkg String::Truncate
+=head3 L<Gravatar::URL>
 
-=pkg Template::Test
+=head3 L<IO::Socket::SSL>
 
-=pkg Test::Pod::Coverage
+=head3 L<IPC::System::Simple>
+
+=head3 L<JavaScript::Minifier>
+
+=head3 L<JavaScript::Minifier::XS>
+
+=head3 L<Modern::Perl>
+
+=head3 L<Pod::Coverage::TrustPod>
+
+=head3 L<SQL::Beautify>
+
+=head3 L<String::Truncate>
+
+=head3 L<Template::Test>
+
+=head3 L<Test::Pod::Coverage>
 
 1;
+
+=head1 AUTHOR
+
+Michael Schout <mschout@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Michael Schout.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/mschout/task-belike-mschout>
+and may be cloned from L<git://github.com/mschout/task-belike-mschout.git>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to bug-task-belike-mschout@rt.cpan.org or through the web interface at:
+ http://rt.cpan.org/Public/Dist/Display.html?Name=Task-BeLike-MSCHOUT
+
+=cut
+
