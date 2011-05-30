b708006192e9fd93ce6b0934ddfcf22b8f268577
Michael Schout <mschout@gkg.net>
2011-05-30 16:21:46 -0500

    initial import

---
+++ lib/Task/BeLike/MSCHOUT.pm
@@ -0,0 +1,65 @@
+package Task::BeLike::MSCHOUT;
+
+use strict;
+use warnings;
+
+# ABSTRACT: be more like MSCHOUT -- use the modules he does!
+
+=pkgroup Useful Command-Line Tools
+
+=pkg App::Ack 1.82 a working --pager
+
+=pkg App::Nopaste 0.28 Gist support via Gist API
+
+=pkg App::cpanminus CPAN improved
+
+=pkg App::cpanoutdated Show outdated CPAN modules
+
+=pkg Perl::Tidy tidy up perl code
+
+=pkg Vimana vim scripts manager
+
+
+=pkggroup Useful CPAN Modules
+
+=pkg Archive::Zip
+
+=pkg BackPAN::Index
+
+=pkg CSS::Minifier
+
+=pkg CSS::Minifier::XS
+
+=pkg DBD::SQLite
+
+=pkg DBI
+
+=pkg Dist::Zilla::Plugin::Twitter
+
+=pkg Dist::Zilla::PluginBundle::MSCHOUT
+
+=pkg Dist::Zilla::PluginBundle::RJBS
+
+=pkg Gravatar::URL
+
+=pkg IO::Socket::SSL
+
+=pkg IPC::System::Simple
+
+=pkg JavaScript::Minifier
+
+=pkg JavaScript::Minifier::XS
+
+=pkg Modern::Perl
+
+=pkg Pod::Coverage::TrustPod
+
+=pkg SQL::Beautify
+
+=pkg String::Truncate
+
+=pkg Template::Test
+
+=pkg Test::Pod::Coverage
+
+1;
